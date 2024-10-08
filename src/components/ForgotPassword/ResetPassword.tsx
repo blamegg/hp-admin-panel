2ab2b9ecc14d63b27b956c54156ae027b4d1c305
Boby-0108 <bobyparashar01.hp@outlook.com>
2024-10-08 15:09:03 +0530

    forgot password

--- src/components/ForgotPassword/ResetPassword.tsx
+++ src/components/ForgotPassword/ResetPassword.tsx
@@ -5,7 +5,8 @@ import React, { FormEvent, useState } from "react";
 import { TextField, Button, IconButton } from "@mui/material";
 import { Visibility, VisibilityOff, CheckCircle } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
-import { FaCheck } from "react-icons/fa";
+// import { FaCheck } from "react-icons/fa";
+import Success from "./Successfull";
 
 export const ResetPassword = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -14,6 +15,7 @@ export const ResetPassword = () => {
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
   const [success, setSuccess] = useState<boolean>(false);
+  const [showSignIn, setShowSignIn] = useState(true);
   const [state, setState] = useState({
     logoVisible: true,
     heading: "Hanging Panda",
@@ -51,91 +53,92 @@ export const ResetPassword = () => {
     }, 2000);
   };
 
+  const handleSignInClick = () => {
+    setShowSignIn(false);
+  };
+
   return (
     <div className="w-full px-2 pb-2 pt-4">
-      <div className="absolute top-[-59px] w-[90%] rounded-xl bg-[#1976D2] py-3">
-        <div className="grid place-items-center">
-          {state.logoVisible && (
-            <Image src={logo.src} alt="company" width={40} height={40} />
-          )}
-        </div>
+      {showSignIn ? (
+        <>
+          <div className="absolute top-[-59px] w-[90%] rounded-xl bg-[#1976D2] py-3">
+            <div className="grid place-items-center">
+              {state.logoVisible && (
+                <Image src={logo.src} alt="company" width={40} height={40} />
+              )}
+            </div>
 
-        <h2 className="mt-1 text-center text-[18px] font-semibold text-white dark:text-black">
-          {state.heading}
-        </h2>
+            <h2 className="mt-1 text-center text-[18px] font-semibold text-white dark:text-black">
+              {state.heading}
+            </h2>
 
-        <h2 className="mt-1 text-center text-[18px] font-semibold text-white dark:text-black">
-          {state.subHeading}
-        </h2>
-      </div>
-
-      <form onSubmit={handleSubmit} className="mt-20">
-        <div className="relative mb-6">
-          <TextField
-            label="Password"
-            type={showPassword ? "text" : "password"}
-            size="small"
-            placeholder="6+ Characters, 1 Capital letter, 1 Number"
-            variant="outlined"
-            fullWidth
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            InputProps={{
-              endAdornment: (
-                <IconButton onClick={handleTogglePassword} edge="end">
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              ),
-            }}
-            error={!!error}
-            helperText={error && password !== "" ? error : ""}
-          />
-        </div>
-
-        <div className="relative mb-6">
-          <TextField
-            label="Confirm Password"
-            type={showConfirmPassword ? "text" : "password"}
-            size="small"
-            placeholder="6+ Characters, 1 Capital letter, 1 Number"
-            variant="outlined"
-            fullWidth
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            InputProps={{
-              endAdornment: (
-                <IconButton onClick={handleToggleConfirmPassword} edge="end">
-                  {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              ),
-            }}
-            error={!!error && confirmPassword !== ""}
-            helperText={error && confirmPassword !== "" ? error : ""}
-          />
-        </div>
-
-        <div className="mb-5">
-          <Button
-            type="submit"
-            variant="contained"
-            className="w-full cursor-pointer rounded-lg border-primary bg-primary py-2 text-white transition hover:bg-opacity-90"
-          >
-            Confirm Password
-          </Button>
-        </div>
-
-        {success && (
-          <div className="flex items-center justify-center space-x-2 rounded-lg p-4 shadow-lg">
-            <span className="rounded-full bg-green-500 p-[5px] text-[15px] text-white">
-              <FaCheck />
-            </span>
-
-            <span className="text-lg font-semibold text-green-700">
-              Password changed successfully! Redirecting...
-            </span>
+            <h2 className="mt-1 text-center text-[18px] font-semibold text-white dark:text-black">
+              {state.subHeading}
+            </h2>
           </div>
-        )}
-      </form>
+          <form onSubmit={handleSubmit} className="mt-20">
+            <div className="relative mb-6">
+              <TextField
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                size="small"
+                placeholder="6+ Characters, 1 Capital letter, 1 Number"
+                variant="outlined"
+                fullWidth
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton onClick={handleTogglePassword} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  ),
+                }}
+                error={!!error}
+                helperText={error && password !== "" ? error : ""}
+              />
+            </div>
+
+            <div className="relative mb-6">
+              <TextField
+                label="Confirm Password"
+                type={showConfirmPassword ? "text" : "password"}
+                size="small"
+                placeholder="6+ Characters, 1 Capital letter, 1 Number"
+                variant="outlined"
+                fullWidth
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      onClick={handleToggleConfirmPassword}
+                      edge="end"
+                    >
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  ),
+                }}
+                error={!!error && confirmPassword !== ""}
+                helperText={error && confirmPassword !== "" ? error : ""}
+              />
+            </div>
+
+            <div className="mb-5">
+              <Button
+                type="submit"
+                onClick={handleSignInClick}
+                variant="contained"
+                className="w-full cursor-pointer rounded-lg border-primary bg-primary py-2 text-white transition hover:bg-opacity-90"
+              >
+                Confirm Password
+              </Button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <Success />
+      )}
     </div>
   );
 };
